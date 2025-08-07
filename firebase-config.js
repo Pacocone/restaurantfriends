0d9aca5cc37504496ea9ed47da1f2b72ada867ae
Pacocone <pacoconesa@vaquelia.com>
2025-08-07 11:59:36 +0200

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,21 @@
-window.FIREBASE_CONFIG = {
-  apiKey: "AIzaSyDULX4PkHaJR35pFtxcNlUKzXJ3lMiJBjM",
-  authDomain: "restaurantworld-86f53.firebaseapp.com",
-  projectId: "restaurantworld-86f53",
-  storageBucket: "restaurantworld-86f53.firebasestorage.app",
-  messagingSenderId: "459185197746",
-  appId: "1:459185197746:web:327c97c4a64f1757b56fab",
-  measurementId: "G-LKKH1T8ZP7"
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCuAfKFNt331_KdfB21dnZRqh7PCINUWQE",
+  authDomain: "restaurantfriends-cf79a.firebaseapp.com",
+  projectId: "restaurantfriends-cf79a",
+  storageBucket: "restaurantfriends-cf79a.firebasestorage.app",
+  messagingSenderId: "127641245441",
+  appId: "1:127641245441:web:e8f5605852df473e8dad00",
+  measurementId: "G-GH262VSY6Y"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
